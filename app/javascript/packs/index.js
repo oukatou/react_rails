c52fdbde02999b28e8794fea9641dcf0e3e49787
whd <530754967@qq.com>
2020-10-18 18:02:26 +0800

    to support jsx

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,6 +5,6 @@ import App from "../components/App";
 document.addEventListener("DOMContentLoaded", () => {
   render(
     <App />,
-    document.body.appendChild(document.createElement("div"))
+    document.getElementById('root')
   );
 });
